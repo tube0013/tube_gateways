97c0601f8cd229d85cee4bbe19336dbf163e3e4f
tube0013 <wpoland@gmail.com>
2021-12-16 23:02:12 -0500

    de-noodle

--- esphome/components/zeroconf/__init__.py
+++ esphome/components/zeroconf/__init__.py
@@ -1,16 +1,14 @@
-from esphome.const import CONF_ID, CONF_SERVICE, CONF_PROTOCOL, CONF_PORT
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.core import CORE
-
-DEPENDENCIES = ["network"]
+from esphome.components import uart
+from esphome.const import CONF_ID, CONF_SERVICE, CONF_PROTOCOL, CONF_PORT, CONF_KEY, CONF_VALUE
 
 MULTI_CONF = True
 
 CONF_TXT = 'txt'
 
-ns = cg.esphome_ns.namespace("zeroconf")
-class_ = ns.class_("Zeroconf", cg.Component)
+ns = cg.esphome_ns.namespace('zeroconf')
+class_ = ns.class_('Zeroconf', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(class_),
@@ -22,13 +20,7 @@ CONFIG_SCHEMA = cv.Schema({
     ),
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    if CORE.using_arduino:
-        if CORE.is_esp32:
-            cg.add_library("ESPmDNS", None)
-        elif CORE.is_esp8266:
-            cg.add_library("ESP8266mDNS", None)
-
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
 
@@ -36,3 +28,6 @@ async def to_code(config):
     
     for key in config.get(CONF_TXT, []):
         cg.add(var.add_txt(key, config[CONF_TXT][key]))
+
+
+
